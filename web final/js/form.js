6dafa862becd09695e9765ae415efe4d4f8c9549
Maaz-9786 <80324179+Maaz-9786@users.noreply.github.com>
2022-04-13 12:41:25 -0400

    Delete web final directory

--- web final/js/form.js
+++
@@ -1,5 +0,0 @@
-function ShowHideDiv() {
-    var chkYes = document.getElementById("hir");
-    var new_field = document.getElementById("rate");
-    new_field.style.display = chkYes.checked ? "block" : "none";
-}
