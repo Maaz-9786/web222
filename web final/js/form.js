6016fcc5cce695d33080d9c1ec43d20d05accf3d
Maaz-9786 <80324179+Maaz-9786@users.noreply.github.com>
2022-04-13 11:56:03 -0400

    Add files via upload

---
+++ web final/js/form.js
@@ -0,0 +1,5 @@
+function ShowHideDiv() {
+    var chkYes = document.getElementById("hir");
+    var new_field = document.getElementById("rate");
+    new_field.style.display = chkYes.checked ? "block" : "none";
+}
